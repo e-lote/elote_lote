9a59f1d7805c890e43f7180a42fb4bee83023099
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2014-12-03 12:39:27 -0300

    [REF] some lote relations to support history

--- lote.py
+++ lote.py
@@ -18,82 +18,111 @@ class product_product(osv.osv):
 product_product()
 
 class product_supplierinfo(osv.osv):
-	_name = "product.supplierinfo"
-	_inherit = "product.supplierinfo"
+    _name = "product.supplierinfo"
+    _inherit = "product.supplierinfo"
 
-	_columns = {
-		'lote_id': fields.many2one('elote.lote', 'Lote'),
-        }
+    _columns = {
+            'lote_id': fields.many2one('elote.lote', 'Lote'),
+    }
 product_supplierinfo()
 
 class elote_lote(osv.osv):
-	_name = "elote.lote"
-	_description = "Lot Administration class"
+    _name = "elote.lote"
+    _description = "Lot Administration class"
+    _inherit = [ 'mail.thread' ]
 
-        def _count_all(self, cr, uid, ids, field_name, arg, context=None):
-           return {
-               obj.id: {
-                   'supplier_ids_count': len(obj.supplier_ids),
-               }
-               for obj in self.browse(cr, uid, ids, context=context)
+    def _count_all(self, cr, uid, ids, field_name, arg, context=None):
+       return {
+           obj.id: {
+               'supplier_ids_count': len(obj.supplier_ids),
+               'order_ids_count': len(obj.order_ids),
            }
+           for obj in self.browse(cr, uid, ids, context=context)
+       }
 
-	_columns = {
-		'name': fields.char('Name',size=32),
-		'sequence_nbr': fields.char('Sequence Nbr',size=32),
-		'date_start': fields.date('Start Date'),
-		'date_end': fields.date('Start End'),
-		'state': fields.selection((('draft','Borrador'),('open','Abierto'),('in_process','Proceso'),('close','Cierre'),('done','Realizado')),'State'),
-	        'supplier_ids': fields.one2many('product.supplierinfo', 'lote_id', 'Products'),
-                'supplier_ids_count': fields.function(_count_all, type='integer', string=_("Supplier Info Count"), multi='_count_all', store=False),
-	        'user_ids': fields.many2many('res.users', 'res_user_rel', 'lote_id', 'user_id', 'Users'),
-		}
-
-	_defaults = {
-		'state': 'draft'
-		}
-
-	def _check_dates(self, cr, uid, ids, context=None):
-		obj = self.browse(cr, uid, ids[0], context=context)
-		if obj.date_start > obj.date_end:
-			return False
-        	return True
-
-        def write(self, cr, uid, ids, vals, context=None):
-		if 'state' in vals.keys():
-			if vals['state'] == 'open':
-				lote_ids = self.search(cr,uid,[('state','=','open')])
-				if len(lote_ids) > 0:
-        		                raise osv.except_osv('Error!', 'Ya existen lotes en estado open.')
-			
-        	return super(elote_lote, self).write(cr, uid, ids, vals, context=context)
-
-        def create(self, cr, uid, vals, context=None):
-		vals['sequence_nbr'] = 'LOTE ' + str(date.today())
-		lote_ids = self.search(cr,uid,[('state','=','open')])
-		if len(lote_ids) > 1:
-                        raise osv.except_osv('Error!', 'Ya existen lotes en estado open.')
-			
-        	return super(elote_lote, self).create(cr, uid, vals, context=context)
-	
-	
-	_constraints = [
-        	(_check_dates, 'Start date should be less than end date.', ['date_start','date_end']),
-    	]
-
-
-        def view_supplier_ids(self, cr, uid, ids, context=None):
-            res_id = ids and ids[0] or False
-            return {
-                'name': _('Supplier Info'),
-                'domain': [('lote_id', 'in', ids)],
-                'res_model': 'product.supplierinfo',
-                'type': 'ir.actions.act_window',
-                'view_id': False,
-                'view_mode': 'tree,form',
-                'view_type': 'form',
-                'limit': 80,
-                'context': "{'default_lote_id': %s}" % (res_id)
+    _columns = {
+            'name': fields.char('Name',size=32),
+            'sequence_nbr': fields.char('Sequence Nbr',size=32),
+            'date_start': fields.date('Start Date'),
+            'date_end': fields.date('Start End'),
+            'state': fields.selection((('draft','Borrador'),('open','Abierto'),('in_process','Proceso'),('close','Cierre'),('done','Realizado')),'State'),
+            'user_ids': fields.many2many('res.users', 'res_user_rel', 'lote_id', 'user_id', 'Users'),
+            'supplier_ids': fields.one2many('product.supplierinfo', 'lote_id', 'Supplier Info'),
+            'supplier_ids_count': fields.function(_count_all, type='integer', string=_("Supplier Info Count"), multi='_count_all', store=False),
+            'order_ids': fields.one2many('purchase.order', 'lote_id', 'Purchase Orders'),
+            'order_ids_count': fields.function(_count_all, type='integer', string=_("Purchase Order Count"), multi='_count_all', store=False),
             }
+
+    _defaults = {
+            'state': 'draft'
+            }
+
+    def _check_dates(self, cr, uid, ids, context=None):
+            obj = self.browse(cr, uid, ids[0], context=context)
+            if obj.date_start > obj.date_end:
+                    return False
+            return True
+
+    def write(self, cr, uid, ids, vals, context=None):
+            if 'state' in vals.keys():
+                    if vals['state'] == 'open':
+                            lote_ids = self.search(cr,uid,[('state','=','open')])
+                            if len(lote_ids) > 0:
+                                    raise osv.except_osv('Error!', 'Ya existen lotes en estado open.')
+                    
+            return super(elote_lote, self).write(cr, uid, ids, vals, context=context)
+
+    def create(self, cr, uid, vals, context=None):
+            vals['sequence_nbr'] = 'LOTE ' + str(date.today())
+            lote_ids = self.search(cr,uid,[('state','=','open')])
+            if len(lote_ids) > 1:
+                    raise osv.except_osv('Error!', 'Ya existen lotes en estado open.')
+                    
+            return super(elote_lote, self).create(cr, uid, vals, context=context)
+    
+    
+    _constraints = [
+            (_check_dates, 'Start date should be less than end date.', ['date_start','date_end']),
+    ]
+
+
+    def view_supplier_ids(self, cr, uid, ids, context=None):
+        res_id = ids and ids[0] or False
+        return {
+            'name': _('Supplier Info'),
+            'domain': [('lote_id', 'in', ids)],
+            'res_model': 'product.supplierinfo',
+            'type': 'ir.actions.act_window',
+            'view_id': False,
+            'view_mode': 'tree,form',
+            'view_type': 'form',
+            'limit': 80,
+            'context': "{'default_lote_id': %s}" % (res_id)
+        }
+
+    def view_order_ids(self, cr, uid, ids, context=None):
+        res_id = ids and ids[0] or False
+        return {
+            'name': _('Purchase Orders'),
+            'domain': [('lote_id', 'in', ids)],
+            'res_model': 'purchase.order',
+            'type': 'ir.actions.act_window',
+            'view_id': False,
+            'view_mode': 'tree,form',
+            'view_type': 'form',
+            'limit': 80,
+            'context': "{'default_lote_id': %s}" % (res_id)
+        }
+
+    def catalog_import(self, cr, uid, ids, context=None):
+        return {
+            'name': _('Catalog Import'),
+            'res_model': 'elote.catalog_import',
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'target': 'new',
+            'context': "{'default_lote_id': %s}" % (ids[0])
+        }
 
 elote_lote()
--- purchase.py
+++ purchase.py
@@ -1,14 +1,25 @@
 from openerp.osv import osv,fields
 from datetime import date
-
+from openerp.tools.translate import _
 
 class purchase_order(osv.osv):
 	_name = "purchase.order"
 	_inherit = "purchase.order"
 
 	_columns = {
-		'lote_id': fields.many2one('elote.lote','Lote'),
+		'lote_id': fields.many2one('elote.lote','Lote',required=True,domain=[('state','=','open')],
+                             states={'confirmed':    [('readonly', True)],
+                                     'approved':     [('readonly', True)],
+                                     'consolidated': [('readonly', True)],
+                                     'not_valid':    [('readonly', True)],
+                                     'in_process':   [('readonly', True)],
+                                     'dispatched':   [('readonly', True)],
+                                     'received':     [('readonly', True)]}),
 		}
+
+        _defaults = {
+            'lote_id': lambda self, cr, uid, context: (self.pool.get('elote.lote').search(cr, uid, [('state','=','open')]) + [False])[0],
+        }
 
 	def _check_valid_lote(self, cr, uid, ids, context=None):
 		obj = self.browse(cr, uid, ids[0], context=context)
@@ -30,33 +41,35 @@ class purchase_order(osv.osv):
 		if invalid_user:
 			return False
 
-		# Valida que el usuario pueda realizar la operacion
+		# Valida que el producto pertenezca al lote
 		invalid_product = True
 		for line in obj.order_line:
-			for product_lote in lote_obj.product_ids:
-				if product_lote.id == line.product_id.id:
-					invalid_product = False
-		if invalid_product:
-			return False
+		    for product_lote in self.pool.get('product.product').search(cr, uid, [], context={'search_default_elote_id': obj.order_id.lote_id.id}):
+                            if product_lote == line.product_id.id:
+                                    invalid_product = False
 
+		# Valida que el producto pertenezca al lote
+		invalid_product = False
+		for line in obj.order_line:
+                    product_lote_ids = self.pool.get('product.product').search(cr, uid, [('id','=',line.product_id.id)], context={'search_default_elote_id': obj.order_id.lote_id.id})
+                    invalid_product = not product_lote_ids
+                    if invalid_product:
+                        import pdb; pdb.set_trace()
+                        message_id = self.message_post(cr, uid, [so.id],
+                                                       subject=_('Request action'),
+                                                       body=_('The product %s is required for this lote.') % (line.product_id.name))
+
+		if invalid_product:
+                    return False
 
         	return True
 
-	#_constraints = [
-        #	(_check_valid_lote, 'Invalid lote.', ['lote_id']),
-    	#]
-
         def create(self, cr, uid, vals, context=None):
-		# sequence_id = self.pool.get('ir.sequence').search('name','=','E-Lote')
-		#sequence_id = self.pool.get('ir.sequence').search('prefix','=','LOT')
-		#if not sequence_id:
-                #        raise osv.except_osv(_('Error!'), _('Please define a sequence for lotes.'))
 		lote_id = self.pool.get('elote.lote').search(cr,uid,[('state','=','open')])
 		if not lote_id:
                         raise osv.except_osv('Error!', 'No hay lotes abiertos para procesar.')
 
 		vals['lote_id'] = lote_id[0]
-		# import pdb;pdb.set_trace()
 		user_obj = self.pool.get('res.users').browse(cr,uid,uid,context=context)
 		vals['dest_address_id'] = user_obj.partner_id.id
 		
@@ -82,13 +95,12 @@ class purchase_order_line(osv.osv):
 	_inherit = "purchase.order.line"
 
 	def _check_lotes(self, cr, uid, ids, context=None):
-		obj = self.browse(cr, uid, ids[0], context=context)
-		return_value = False
-		for product in obj.order_id.lote_id.product_ids:
-			if product.id == obj.product_id.id:
-				return_value = True
-				break
-        	return return_value
+            assert len(ids) == 1, "Only works by one line"
+            obj = self.browse(cr, uid, ids[0], context=context)
+            return len(self.pool.get('product.product').search(cr, uid,
+                                                               [('id','=',obj.product_id.id)],
+                                                               context={'search_default_elote_id': obj.order_id.lote_id.id})
+                      ) == 1
 
 	_constraints = [
         	(_check_lotes, 'Product should be included in lote', ['product_id']),
@@ -96,34 +108,3 @@ class purchase_order_line(osv.osv):
 
 purchase_order_line()
 
-"""
-class elote_lote(osv.osv):
-	_name = "elote.lote"
-	_description = "Lot Administration class"
-
-	_columns = {
-		'name': fields.char('Name',size=32),
-		'date_start': fields.date('Start Date'),
-		'date_end': fields.date('Start End'),
-		'state': fields.selection((('draft','Draft'),('in_process','In Process'),('done','Done')),'State'),
-	        'product_ids': fields.many2many('product.product', 'product_lote_rel', 'lote_id', 'product_id', 'Products'),
-	        'user_ids': fields.many2many('res.users', 'res_user_rel', 'lote_id', 'user_id', 'Users'),
-		}
-
-	_defaults = {
-		'state': 'draft'
-		}
-
-	def _check_dates(self, cr, uid, ids, context=None):
-		obj = self.browse(cr, uid, ids[0], context=context)
-		if obj.date_start > obj.date_end:
-			return False
-        	return True
-
-	_constraints = [
-        	(_check_dates, 'Start date should be less than end date.', ['date_start','date_end']),
-    	]
-
-elote_lote()
-
-"""
